ccdd426ae629efbb3d19652d63354682d874d828
WontonNice <nathanim560@gmail.com>
2025-10-07 14:25:30 -0400

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000', // Local dev backend
+        changeOrigin: true,
+      },
+    },
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['ccm-attendancetracker.onrender.com'],
+
+  },
+});
