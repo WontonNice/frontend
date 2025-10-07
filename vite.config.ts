6728f96f7e8674c391380ec716802e8202975ef0
WontonNice <nathanim560@gmail.com>
2025-10-07 14:45:39 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4173,
-    allowedHosts: ['ccm-attendancetracker.onrender.com'],
+    allowedHosts: ['frontend-tgl3.onrender.com'],
 
   },
 });
