bfe4be0d66a337355788b9b802136fb6faa83027
WontonNice <nathan.im@stonybrook.edu>
2025-10-09 20:08:37 -0400

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 // src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 import Register from "./components/Register";
 
 export default function App() {
-  return <Register />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Default route → Login page */}
+        <Route path="/" element={<Login />} />
+
+        {/* Create Account route */}
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
