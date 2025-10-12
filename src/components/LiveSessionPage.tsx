372b7c9773746a2ca2b0694231422261ed581e7d
WontonNice <nathan.im@stonybrook.edu>
2025-10-12 12:16:15 -0400

    d

--- src/components/LiveSessionPage.tsx
+++ src/components/LiveSessionPage.tsx
@@ -368,7 +368,6 @@ export default function LiveSessionPage() {
         className="relative w-full h-[calc(100vh-var(--topbar-height))] bg-white overflow-hidden touch-none"
         style={{ WebkitUserSelect: "none", userSelect: "none" }}
       >
-        <canvas ref={canvasRef} className="absolute inset-0 block" />
 
         {/* images (draggable + resizable) */}
         {images.map((img) => {
@@ -391,6 +390,8 @@ export default function LiveSessionPage() {
             </div>
           );
         })}
+
+        <canvas ref={canvasRef} className="absolute inset-0 block" />
 
         {/* live cursors */}
         <div className="absolute inset-0 pointer-events-none">
